f34ac34c66f91fbd021bbcdd6ed0bbac5c9c54a2
Your Name <you@example.com>
2020-10-13 23:26:08 +0200

    femib-ng

--- src/affine/affine.cpp
+++ src/affine/affine.cpp
@@ -1,7 +1,3 @@
-/*
-*	affine.cpp
-*/
-
 #include "affine.hpp"
 
 dmat affineB(const dtrian &t) {
@@ -11,6 +7,7 @@ dmat affineB(const dtrian &t) {
 	t[1](1) - t[0](1), t[2](1) - t[0](1);
     return B;
 }
+
 dvec affineb(const dtrian &t) {
     dvec b = t[0];
     return b;
@@ -26,19 +23,3 @@ dvec affine_inv(const dtrian &t, const dvec &x) {
     return y;
 }
 
-std::function<dvec(const dvec&)> affine(const dtrian &t) {
-	return [&t](const dvec &x){
-		return affine(t, x);
-	};
-}
-
-/*std::function<dvec(const dvec&)> affine_inv(const dtrian &t) {
-	
-	return [&t](const dvec &x){
-		dmat B;
-		B << 	t[1](0) - t[0](0), t[2](0) - t[0](0),
-		   	t[1](1) - t[0](1), t[2](1) - t[0](1);
-		dvec b = t[0];
-		return B*x+b;
-	};
-}*/
