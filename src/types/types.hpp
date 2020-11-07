9d63fd6051fd9146a09199c2dfbfe92b57c340f4
Your Name <you@example.com>
2020-11-07 21:53:28 +0100

    femib-ng

--- src/types/types.hpp
+++ src/types/types.hpp
@@ -1,6 +1,7 @@
 #ifndef FEMIB_TYPES_HPP
 #define FEMIB_TYPES_HPP
 
+#include "iterator_tpl.h"
 #include <Eigen/Core>
 #include <vector>
 
@@ -9,7 +10,43 @@ namespace femib::types {
 template <typename T, int d> using dvec = Eigen::Matrix<T, d, 1>;
 template <typename T, int d> using dmat = Eigen::Matrix<T, d, d>;
 template <typename T, int d> using dtrian = std::vector<dvec<T, d>>;
-template <typename T, int d> using mesh = std::vector<dtrian<T, d>>;
+template <int d> using ditrian = Eigen::Matrix<int, d+1, 1>;
+//template <typename T, int d> using mesh = std::vector<dtrian<T, d>>;
+template <typename f, int d> 
+struct mesh {
+  std::vector<dvec<f, d>> P;
+  std::vector<ditrian<d>> T;
+  std::vector<int> E;
 
+  typedef ditrian<d> ditriand;
+  typedef dtrian<f,d> dtrianfd;
+  STL_TYPEDEFS(ditriand);
+  struct it_state {
+    int pos;
+    inline void next(const mesh<f,d>* ref) { ++pos; }
+    inline void begin(const mesh<f,d>* ref) { pos = 0; }
+    inline void end(const mesh<f,d>* ref) { pos = ref->T.size(); }
+    inline dtrianfd get(mesh<f,d>* ref) { 
+	    dtrianfd t;
+	    t.reserve(d+1);
+	    for(int tt : ref->T[pos]){
+		t.emplace_back(ref->P[tt]);
+	    }
+	    return t;
+	    //return {ref->P[(ref->T[pos])[0]],ref->P[(ref->T[pos])[1]],ref->P[(ref->T[pos])[2]]};
+    }
+    inline const dtrianfd get(const mesh<f,d>* ref) {
+	    dtrianfd t;
+	    t.reserve(d+1);
+	    for(int tt : ref->T[pos]){
+		t.emplace_back(ref->P[tt]);
+	    }
+	    return t;
+	    //return {ref->P[(ref->T[pos])[0]],ref->P[(ref->T[pos])[1]],ref->P[(ref->T[pos])[2]]};
+    }
+    inline bool cmp(const it_state& s) const { return pos != s.pos; }
+  };
+  SETUP_ITERATORS(mesh, dtrianfd, it_state);
+};
 } // namespace femib::types
 #endif
--- test/mesh_test.cpp
+++ test/mesh_test.cpp
@@ -12,17 +12,38 @@ TEST_CASE("testing mesh") {
   femib::gauss::node<float, 2> node3 = {1.0 / 6.0, {2.0 / 3.0, 1.0 / 6.0}};
   femib::gauss::rule<float, 2> rule = {{node1, node2, node3}};
 
-  femib::types::mesh<float, 2> mesh = {{{0.0,0.0},{1.0,0.0},{0.5,0.5}},
-{{1.0,0.0},{1.0,1.0},{0.5,0.5}},
-{{1.0,1.0},{0.0,1.0},{0.5,0.5}},{{0.0,1.0},{0.0,0.0},{0.5,0.5}}};
+  femib::types::mesh<float, 2> mesh = {
+	  // P
+	  {
+		{0.0,0.0},{1.0,0.0},{1.0,1.0},{0.0,1.0},{0.5,0.5}
+	  },
+	  // T
+	  {
+		{0,1,4},{1,2,4},{2,3,4},{3,0,4}
 
+	  },
+	  // E
+	  {
+		  0,1,2,3
+	  }
+  };
+
+	  //,{{{0.0,0.0},{1.0,0.0},{0.5,0.5}},{{1.0,0.0},{1.0,1.0},{0.5,0.5}},
+//{{1.0,1.0},{0.0,1.0},{0.5,0.5}},{{0.0,1.0},{0.0,0.0},{0.5,0.5}}},{}};
+{
   std::function<float(femib::types::dvec<float, 2>)> f =
       [](femib::types::dvec<float, 2> x) { return 1; };
   float area = femib::mesh::integrate<float, 2>(rule, f, mesh);
   CHECK(std::fabs(area - 1.0 ) <= EPSILON);
-
-  std::function<float(femib::types::dvec<float, 2>)> g =
+}{
+  std::function<float(femib::types::dvec<float, 2>)> f =
       [](femib::types::dvec<float, 2> x) { return x(0) + x(1); };
-  float integral = femib::mesh::integrate<float, 2>(rule, g, mesh);
+  float integral = femib::mesh::integrate<float, 2>(rule, f, mesh);
   CHECK(std::fabs(integral - 1.0 ) <= EPSILON);
+}{
+  std::function<float(femib::types::dvec<float, 2>)> f =
+      [](femib::types::dvec<float, 2> x) { return x(0) + x(1) + 1; };
+  float integral = femib::mesh::integrate<float, 2>(rule, f, mesh);
+  CHECK(std::fabs(integral - 2.0 ) <= EPSILON);
+}
 }
