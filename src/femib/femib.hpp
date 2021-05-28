be8c1805f8e709e506171b7ed2c761640f72ae9e
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-05-28 23:00:40 +0200

    femib

---
+++ src/femib/femib.hpp
@@ -0,0 +1,6 @@
+#ifndef FEMIB_HPP_INCLUDED_
+#define FEMIB_HPP_INCLUDED_
+namespace femib::femib {
+
+}
+#endif
---
+++ test/femib_test.cpp
@@ -0,0 +1,7 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "../src/femib/femib.hpp"
+
+TEST_CASE("testing femib") {
+    CHECK(true);
+}
