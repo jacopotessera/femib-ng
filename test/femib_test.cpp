091fb6d3bc70f01e2c3512d5672998917b7c4c83
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-05-29 15:54:10 +0200

    femib

--- test/femib_test.cpp
+++ test/femib_test.cpp
@@ -18,6 +18,27 @@ int get_index(const femib::types::nodes<float, 2> &nodes, int i, int n) {
   return nodes.T[n][i];
 }
 
+auto printNode_generator(
+    femib::finite_element_space::finite_element_space<float, 2, 1> s,
+    Eigen::Matrix<float, Eigen::Dynamic, 1> xx) {
+
+  return [&s, &xx](std::vector<int> t) {
+    int j_0 = t[0];
+    int j_1 = t[1];
+    int j_2 = t[2];
+    std::cout << s.nodes.P[j_0](0) << "\t" << s.nodes.P[j_0](1) << "\t"
+              << xx(j_0) << std::endl;
+    std::cout << s.nodes.P[j_1](0) << "\t" << s.nodes.P[j_1](1) << "\t"
+              << xx(j_1) << std::endl;
+    std::cout << s.nodes.P[j_2](0) << "\t" << s.nodes.P[j_2](1) << "\t"
+              << xx(j_2) << std::endl;
+    std::cout << s.nodes.P[j_0](0) << "\t" << s.nodes.P[j_0](1) << "\t"
+              << xx(j_0) << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+  };
+}
+
 int main() {
 
   femib::gauss::rule<float, 2> rule =
@@ -54,8 +75,6 @@ int main() {
                 return (affineBinv(t) * f.base_functions[j].dx(
                                             affineBinv(t) * (x - affineb(t))));
               };
-              // float m = femib::mesh::integrate<float,2>(rule,
-              // [](femib::types::dvec<float,2>){return (float)1.0;}, t);
               float m = femib::mesh::integrate<float, 2>(
                   rule,
                   [&t, &f, i, j](femib::types::dvec<float, 2> x) {
@@ -75,6 +94,33 @@ int main() {
                     // return a.dx(x)[0]*b.dx(x)[0] + a.dx(x)[1]*b.dx(x)[1];
                   },
                   t);
+              MM.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n),
+                                                 get_index(s.nodes, j, n), m));
+            }
+          }
+        }
+        return MM;
+      };
+
+  poisson.f =
+      [&f, &rule](
+          femib::finite_element_space::finite_element_space<float, 2, 1> s) {
+        std::vector<Eigen::Triplet<float>> F;
+        for (int n = 0; n < s.mesh.T.size(); ++n) {
+          for (int i = 0; i < s.finite_element.base_functions.size(); ++i) {
+            for (int j = 0; j < s.finite_element.base_functions.size(); ++j) {
+              femib::types::F<float, 2, 1> a;
+              femib::types::F<float, 2, 1> b;
+              femib::types::dtrian<float, 2> t = s.mesh[n];
+              a.dx = [&](const femib::types::dvec<float, 2> &x) {
+                return (affineBinv(t) * f.base_functions[i].dx(
+                                            affineBinv(t) * (x - affineb(t))));
+              };
+              b.dx = [&](const femib::types::dvec<float, 2> &x) {
+                return (affineBinv(t) * f.base_functions[j].dx(
+                                            affineBinv(t) * (x - affineb(t))));
+              };
+
               float f_ = femib::mesh::integrate<float, 2>(
                   rule,
                   [&t, &f, i](femib::types::dvec<float, 2> x) {
@@ -84,78 +130,83 @@ int main() {
                     return a_0;
                   },
                   t);
-              // std::cout << "n: " << n << ", i: " << i << ", j: " << j
-              //          << " -> m: " << m << std::endl;
-              // std::cout << "n: " << n << ", i: " << i << ", j: " << j
-              //          << " -> f: " << f_ << std::endl;
-              MM.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n),
-                                                 get_index(s.nodes, j, n), m));
-              // F.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n), 0,
-              // f_));
+              F.push_back(
+                  Eigen::Triplet<float>(get_index(s.nodes, i, n), 0, f_));
             }
           }
         }
-        return MM;
+        return F;
       };
 
-  poisson.f = [&f, &rule](
-                  femib::finite_element_space::finite_element_space<float, 2, 1>
-                      s) {
-    std::vector<Eigen::Triplet<float>> F;
-    for (int n = 0; n < s.mesh.T.size(); ++n) {
-      for (int i = 0; i < s.finite_element.base_functions.size(); ++i) {
-        for (int j = 0; j < s.finite_element.base_functions.size(); ++j) {
-          femib::types::F<float, 2, 1> a;
-          femib::types::F<float, 2, 1> b;
-          femib::types::dtrian<float, 2> t = s.mesh[n];
-          a.dx = [&](const femib::types::dvec<float, 2> &x) {
-            return (affineBinv(t) *
-                    f.base_functions[i].dx(affineBinv(t) * (x - affineb(t))));
-          };
-          b.dx = [&](const femib::types::dvec<float, 2> &x) {
-            return (affineBinv(t) *
-                    f.base_functions[j].dx(affineBinv(t) * (x - affineb(t))));
-          };
-          // float m = femib::mesh::integrate<float,2>(rule,
-          // [](femib::types::dvec<float,2>){return (float)1.0;}, t);
-          float m = femib::mesh::integrate<float, 2>(
-              rule,
-              [&t, &f, i, j](femib::types::dvec<float, 2> x) {
-                float a_0 =
-                    (affineBinv(t) * f.base_functions[i].dx(
-                                         affineBinv(t) * (x - affineb(t))))[0];
-                float a_1 =
-                    (affineBinv(t) * f.base_functions[i].dx(
-                                         affineBinv(t) * (x - affineb(t))))[1];
-                float b_0 =
-                    (affineBinv(t) * f.base_functions[j].dx(
-                                         affineBinv(t) * (x - affineb(t))))[0];
-                float b_1 =
-                    (affineBinv(t) * f.base_functions[j].dx(
-                                         affineBinv(t) * (x - affineb(t))))[1];
-                return a_0 * b_0 + a_1 * b_1;
-                // return a.dx(x)[0]*b.dx(x)[0] + a.dx(x)[1]*b.dx(x)[1];
-              },
-              t);
-          float f_ = femib::mesh::integrate<float, 2>(
-              rule,
-              [&t, &f, i](femib::types::dvec<float, 2> x) {
-                float a_0 = -40 * x(0) * x(1) *
-                            (f.base_functions[i].x(affineBinv(t) *
-                                                   (x - affineb(t))))[0];
-                return a_0;
-              },
-              t);
-          // std::cout << "n: " << n << ", i: " << i << ", j: " << j
-          //          << " -> m: " << m << std::endl;
-          // std::cout << "n: " << n << ", i: " << i << ", j: " << j
-          //          << " -> f: " << f_ << std::endl;
-          // MM.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n),
-          //                                  get_index(s.nodes, j, n), m));
-          F.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n), 0, f_));
-        }
-      }
+  std::vector<Eigen::Triplet<float>> M = poisson.M(s, s);
+  std::vector<Eigen::Triplet<float>> F = poisson.f(s);
+
+  std::vector<Eigen::Triplet<float>> B;
+
+  std::function<float(femib::types::dvec<float, 2>)> b =
+      [](const femib::types::dvec<float, 2> &x) { return x(0) + x(1); };
+  //[](const femib::types::dvec<float, 2> &x) { return 1.0; };
+  for (int e : s.nodes.E) {
+    // std::cout << e << ": " << b(s.nodes.P[e]) << std::endl;
+    B.push_back(Eigen::Triplet<float>(e, 0, b(s.nodes.P[e])));
+  }
+
+  std::vector<int> not_edges;
+  for (int i = 0; i < s.nodes.P.size(); i++) {
+    if (std::find(s.nodes.E.begin(), s.nodes.E.end(), i) == s.nodes.E.end()) {
+      not_edges.push_back(i);
     }
-    return F;
-  };
+  }
+
+  Eigen::SparseMatrix<float> sB =
+      Eigen::SparseMatrix<float>(s.nodes.P.size(), 1);
+  sB.setFromTriplets(B.begin(), B.end());
+  Eigen::Matrix<float, Eigen::Dynamic, 1> dB =
+      Eigen::Matrix<float, Eigen::Dynamic, 1>(sB);
+  Eigen::SparseMatrix<float> sM =
+      Eigen::SparseMatrix<float>(s.nodes.P.size(), s.nodes.P.size());
+  sM.setFromTriplets(M.begin(), M.end());
+  Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> dM =
+      Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>(sM);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> ss =
+      dM(Eigen::all, Eigen::all) * dB(Eigen::all, Eigen::all);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> mf;
+  mf.resize(s.nodes.P.size(), 1);
+  for (int i = 0; i < s.nodes.P.size(); i++) {
+    auto k = std::find(not_edges.begin(), not_edges.end(), i);
+    mf(i, 0) = 0.0;
+    if (k != not_edges.end()) {
+      mf(i, 0) = ss(k - not_edges.begin(), 0);
+    }
+  }
+
+  Eigen::SparseMatrix<float> sF =
+      Eigen::SparseMatrix<float>(s.nodes.P.size(), 1);
+  sF.setFromTriplets(F.begin(), F.end());
+  Eigen::Matrix<float, Eigen::Dynamic, 1> dF =
+      Eigen::Matrix<float, Eigen::Dynamic, 1>(sF);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> bbb = (dF - ss)(not_edges, 0);
+  Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> AAA =
+      (dM)(not_edges, not_edges);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> xxx =
+      AAA.colPivHouseholderQr().solve(bbb);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> xx;
+  xx.resize(s.nodes.P.size(), 1);
+
+  for (int i = 0; i < s.nodes.P.size(); i++) {
+    xx(i, 0) = 0.0;
+    auto k = std::find(not_edges.begin(), not_edges.end(), i);
+    if (k != not_edges.end()) {
+      xx(i, 0) = xxx(k - not_edges.begin(), 0);
+    }
+    auto kk = std::find(s.nodes.E.begin(), s.nodes.E.end(), i);
+    if (kk != s.nodes.E.end()) {
+      xx(i, 0) = dB(i, 0);
+    }
+  }
+
+  if (true) {
+    std::for_each(s.nodes.T.begin(), s.nodes.T.end(),
+                  printNode_generator(s, xx));
+  }
 }
