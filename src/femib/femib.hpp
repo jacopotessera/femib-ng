1d930869c259755f5f15ea3dccbca6c12f14bb8c
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-05-31 01:11:56 +0200

    tbb

--- src/femib/femib.hpp
+++ src/femib/femib.hpp
@@ -5,13 +5,13 @@
 #include "../finite_element_space/finite_element_space.hpp"
 #include "../gauss/gauss.hpp"
 #include "../mesh/mesh.hpp"
+#include "tbb/task_scheduler_init.h"
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
-#include <tbb/concurrent_vector.h>
 #include <functional>
-#include <vector>
+#include <tbb/concurrent_vector.h>
 #include <tbb/tbb.h>
-#include "tbb/task_scheduler_init.h"
+#include <vector>
 
 int get_index(const femib::types::nodes<float, 2> &nodes, int i, int n) {
   return nodes.T[n][i];
@@ -39,8 +39,13 @@ ddot(femib::types::F<float, 2, 1> a, femib::types::F<float, 2, 1> b) {
   };
 }
 
+template <typename T> struct MeF {
+  std::vector<Eigen::Triplet<T>> M;
+  std::vector<Eigen::Triplet<T>> F;
+};
+
 template <typename T, int d, int e>
-std::vector<Eigen::Triplet<T>>
+MeF<T>
 build_diagonal(femib::finite_element_space::finite_element_space<T, d, e> s,
                femib::gauss::rule<T, d> rule,
 
@@ -53,42 +58,61 @@ build_diagonal(femib::finite_element_space::finite_element_space<T, d, e> s,
                    >
                    fff) {
 
-  std::vector<Eigen::Triplet<float>> MM;
-  tbb::concurrent_vector<Eigen::Triplet<float>> cMM;
-  
-  int N_THREADS = tbb::task_scheduler_init::default_num_threads();
-  tbb::task_scheduler_init init(N_THREADS);
-  
-  for (int n = 0; n < s.mesh.T.size(); ++n) {
-    for (int i = 0; i < s.finite_element.base_functions.size(); ++i) {
-      for (int j = 0; j < s.finite_element.base_functions.size(); ++j) {
-        femib::types::F<float, 2, 1> a;
-        femib::types::F<float, 2, 1> b;
-        femib::types::dtrian<float, 2> t = s.mesh[n];
-        a.dx = [&](const femib::types::dvec<float, 2> &x) {
-          return (femib::affine::affineBinv(t) *
-                  s.finite_element.base_functions[i].dx(
-                      femib::affine::affineBinv(t) *
-                      (x - femib::affine::affineb(t))));
-        };
-        b.dx = [&](const femib::types::dvec<float, 2> &x) {
-          return (femib::affine::affineBinv(t) *
-                  s.finite_element.base_functions[j].dx(
-                      femib::affine::affineBinv(t) *
-                      (x - femib::affine::affineb(t))));
-        };
-        float m = femib::mesh::integrate<float, 2>(
-            rule,
-            //[&t, &f, i, j, &a, &b](femib::types::dvec<float, 2> x) {
-            //  return a.dx(x)[0] * b.dx(x)[0] + a.dx(x)[1] * b.dx(x)[1];
-            //},
-            fff(a, b), t);
-        cMM.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n),
-                                           get_index(s.nodes, j, n), m));
-      }
-    }
-  }
-  return {cMM.begin(), cMM.end()};
+  tbb::task_scheduler_init init(tbb::task_scheduler_init::automatic);
+  tbb::concurrent_vector<Eigen::Triplet<float>> cMM(
+      s.mesh.T.size() * s.finite_element.base_functions.size() *
+      s.finite_element.base_functions.size());
+
+  tbb::concurrent_vector<Eigen::Triplet<float>> F(
+      s.mesh.T.size() * s.finite_element.base_functions.size() *
+      s.finite_element.base_functions.size());
+
+  tbb::parallel_for(
+      tbb::blocked_range<int>(0, s.mesh.T.size()),
+      [&](const tbb::blocked_range<int> &range) {
+        for (int n = range.begin(); n < range.end(); ++n) {
+          femib::types::dtrian<float, 2> t = s.mesh[n];
+          for (int i = 0; i < s.finite_element.base_functions.size(); ++i) {
+            femib::types::F<float, 2, 1> a;
+
+            a.dx = [&](const femib::types::dvec<float, 2> &x) {
+              return (femib::affine::affineBinv(t) *
+                      s.finite_element.base_functions[i].dx(
+                          femib::affine::affineBinv(t) *
+                          (x - femib::affine::affineb(t))));
+            };
+
+            for (int j = 0; j < s.finite_element.base_functions.size(); ++j) {
+
+              femib::types::F<float, 2, 1> b;
+
+              b.dx = [&](const femib::types::dvec<float, 2> &x) {
+                return (femib::affine::affineBinv(t) *
+                        s.finite_element.base_functions[j].dx(
+                            femib::affine::affineBinv(t) *
+                            (x - femib::affine::affineb(t))));
+              };
+              float m = femib::mesh::integrate<float, 2>(rule, fff(a, b), t);
+              cMM.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n),
+                                                  get_index(s.nodes, j, n), m));
+            }
+
+            float f_ = femib::mesh::integrate<float, 2>(
+                rule,
+                [&t, &s, i](femib::types::dvec<float, 2> x) {
+                  float a_0 = -40 * x(0) * x(1) *
+                              (s.finite_element.base_functions[i].x(
+                                  femib::affine::affineBinv(t) *
+                                  (x - femib::affine::affineb(t))))[0];
+                  return a_0;
+                },
+                t);
+            F.push_back(Eigen::Triplet<float>(get_index(s.nodes, i, n), 0, f_));
+          }
+        }
+      });
+
+  return {{cMM.begin(), cMM.end()}, {F.begin(), F.end()}};
 }
 
 } // namespace femib::poisson
--- test/femib_test.cpp
+++ test/femib_test.cpp
@@ -41,7 +41,7 @@ int main() {
       femib::gauss::create_gauss_2_2d<float, 2>();
   std::string mesh_dir = MESH_DIR;
   femib::types::mesh<float, 2> mesh = femib::mesh::read<float, 2>(
-      mesh_dir + "p4.mat", mesh_dir + "t4.mat", mesh_dir + "e4.mat");
+      mesh_dir + "p5.mat", mesh_dir + "t5.mat", mesh_dir + "e5.mat");
 
   femib::finite_element::finite_element<float, 2, 1> f =
       femib::finite_element::create_finite_element_P1_2d1d<float, 2, 1>();
@@ -121,10 +121,11 @@ int main() {
         return F;
       };
 
-  std::vector<Eigen::Triplet<float>> M =
-      femib::poisson::build_diagonal<float, 2, 1>(
-          s, rule, femib::poisson::ddot<float, 2, 1>); // poisson.M(s, s);
-  std::vector<Eigen::Triplet<float>> F = poisson.f(s);
+  femib::poisson::MeF<float> mef = femib::poisson::build_diagonal<float, 2, 1>(
+      s, rule, femib::poisson::ddot<float, 2, 1>); // poisson.M(s, s);
+
+  std::vector<Eigen::Triplet<float>> M = mef.M;
+  std::vector<Eigen::Triplet<float>> F = mef.F; // poisson.f(s);
 
   std::vector<Eigen::Triplet<float>> B;
 
