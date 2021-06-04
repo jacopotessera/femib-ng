ba955ed64054de9b227c5b2af56305408df16071
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-06-05 00:28:34 +0200

    stokes wip

--- src/mesh/mesh.hpp
+++ src/mesh/mesh.hpp
@@ -16,24 +16,7 @@ namespace femib::mesh {
 template <typename T, int d>
 T integrate(const femib::gauss::rule<T, d> &rule,
             const std::function<T(femib::types::dvec<T, d>)> &f,
-            const femib::types::dtrian<T, d> &t);
-template <typename T, int d>
-T integrate(const femib::gauss::rule<T, d> &rule,
-            const std::function<T(femib::types::dvec<T, d>)> &f,
-            const femib::types::mesh<T, d> &mesh);
-template <typename T, int d>
-femib::types::mesh<T, d> read(const std::string &filename_p,
-                              const std::string &filename_t,
-                              const std::string &filename_e);
-
-template <typename T, int d>
-std::vector<femib::types::dtrian<T, d>>
-init(const femib::types::mesh<T, d> &mesh);
-
-template <typename T, int d>
-T femib::mesh::integrate(const femib::gauss::rule<T, d> &rule,
-                         const std::function<T(femib::types::dvec<T, d>)> &f,
-                         const femib::types::dtrian<T, d> &t) {
+            const femib::types::dtrian<T, d> &t) {
 
   std::function<T(femib::types::dvec<T, d>)> g =
       [&t, &f](const femib::types::dvec<T, d> &x) {
@@ -43,9 +26,9 @@ T femib::mesh::integrate(const femib::gauss::rule<T, d> &rule,
 }
 
 template <typename T, int d>
-T femib::mesh::integrate(const femib::gauss::rule<T, d> &rule,
-                         const std::function<T(femib::types::dvec<T, d>)> &f,
-                         const femib::types::mesh<T, d> &mesh) {
+T integrate(const femib::gauss::rule<T, d> &rule,
+            const std::function<T(femib::types::dvec<T, d>)> &f,
+            const femib::types::mesh<T, d> &mesh) {
   auto unary_op = [&rule, &f](const femib::types::dtrian<T, d> &t) {
     return femib::mesh::integrate(rule, f, t);
   };
@@ -54,9 +37,9 @@ T femib::mesh::integrate(const femib::gauss::rule<T, d> &rule,
 }
 
 template <typename T, int d>
-femib::types::mesh<T, d> femib::mesh::read(const std::string &filename_p,
-                                           const std::string &filename_t,
-                                           const std::string &filename_e) {
+femib::types::mesh<T, d> read(const std::string &filename_p,
+                              const std::string &filename_t,
+                              const std::string &filename_e) {
   femib::types::mesh<T, d> mesh =
       read_mesh_file<T, d>(filename_p, filename_t, filename_e);
   return mesh;
