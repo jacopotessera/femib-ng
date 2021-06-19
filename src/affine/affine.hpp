cd32c7210f3bf6c5c1f7c0fb30cfde65bef0db37
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-06-19 20:23:57 +0200

    femib

--- src/affine/affine.hpp
+++ src/affine/affine.hpp
@@ -10,10 +10,22 @@
 namespace femib::affine {
 
 template <typename T, int d>
+femib::types::dvec<T, d> affineb(const femib::types::dtrian<T, d> &t) {
+  return t[0];
+}
+
+template <typename T, int d>
 femib::types::dmat<T, d> affineB(const femib::types::dtrian<T, d> &t) {
   femib::types::dmat<T, d> B;
-  B << t[1](0) - t[0](0), t[2](0) - t[0](0), t[1](1) - t[0](1),
-      t[2](1) - t[0](1);
+  if (d == 1)
+    B << t[1](0) - t[0](0);
+  if (d == 2)
+    B << t[1](0) - t[0](0), t[2](0) - t[0](0), t[1](1) - t[0](1),
+        t[2](1) - t[0](1);
+  if (d == 3)
+    B << t[1](0) - t[0](0), t[2](0) - t[0](0), t[3](0) - t[0](0),
+        t[1](1) - t[0](1), t[2](1) - t[0](1), t[3](1) - t[0](1),
+        t[1](2) - t[0](2), t[2](2) - t[0](2), t[3](2) - t[0](2);
   return B;
 }
 
@@ -27,23 +39,15 @@ template <typename T, int d> T affineBdet(const femib::types::dtrian<T, d> &t) {
 }
 
 template <typename T, int d>
-femib::types::dvec<T, d> affineb(const femib::types::dtrian<T, d> &t) {
-  femib::types::dvec<T, d> b = t[0];
-  return b;
-}
-
-template <typename T, int d>
 femib::types::dvec<T, d> affine(const femib::types::dtrian<T, d> &t,
                                 const femib::types::dvec<T, d> &x) {
-  femib::types::dvec<T, d> y = affineB(t) * x + affineb(t);
-  return y;
+  return affineB(t) * x + affineb(t);
 }
 
 template <typename T, int d>
 femib::types::dvec<T, d> affine_inv(const femib::types::dtrian<T, d> &t,
                                     const femib::types::dvec<T, d> &x) {
-  femib::types::dvec<T, d> y = affineB(t).inverse() * (x - affineb(t));
-  return y;
+  return affineB(t).inverse() * (x - affineb(t));
 }
 
 } // namespace femib::affine
