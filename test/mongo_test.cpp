095562eb3856887c00dda212a85e39478b193c27
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-06-22 22:36:02 +0200

    mongo

--- test/mongo_test.cpp
+++ test/mongo_test.cpp
@@ -5,16 +5,13 @@
 TEST_CASE("testing mongo") {
 
   std::vector<std::vector<std::vector<double>>> u = {
-      {{0, 0, 0}, {0.5, 0, 1}, {1, 0, 0}},
-      {{0, 0.5, 1}, {0.5, 0.5, 2}, {1, 0.5, 1}},
-      {{0, 1, 0}, {0.5, 1, 1}, {1, 1, 0}}};
-  std::vector<std::vector<std::vector<double>>> q = {
-      {{0, 0, 0}, {0.5, 0, 1}, {1, 0, 0}},
-      {{0, 0.5, 1}, {0.5, 0.5, 2}, {1, 0.5, 1}},
-      {{0, 1, 0}, {0.5, 1, 1}, {1, 1, 0}}};
+      {{0, 0}, {0, 0}},   {{0.5, 0}, {1, 1}},   {{1, 0}, {0, 0}},
+      {{0, 0.5}, {1, 1}}, {{0.5, 0.5}, {2, 2}}, {{1, 0.5}, {1, 1}},
+      {{0, 1}, {0, 0}},   {{0.5, 1}, {1, 1}},   {{1, 1}, {0, 0}}};
+  std::vector<std::vector<std::vector<double>>> q;
   std::vector<std::vector<std::vector<double>>> x;
 
-  femib::mongo::plot_data p = {"12345", 2, u, q, x};
+  femib::mongo::plot_data p = {"12345", 4, u, q, x};
   femib::mongo::save_plot_data("femib_test", p);
   CHECK(true);
 }
