7b092f2341b02d099b79b071d916f6e547eb66b9
Jacopo Tessera <iacopo.2.tessera.external@atos.net>
2021-06-22 22:22:22 +0200

    mongo

--- test/mongo_test.cpp
+++ test/mongo_test.cpp
@@ -2,4 +2,19 @@
 #include "../src/mongo/mongo.hpp"
 #include <doctest/doctest.h>
 
-TEST_CASE("testing mongo") { CHECK(true); }
+TEST_CASE("testing mongo") {
+
+  std::vector<std::vector<std::vector<double>>> u = {
+      {{0, 0, 0}, {0.5, 0, 1}, {1, 0, 0}},
+      {{0, 0.5, 1}, {0.5, 0.5, 2}, {1, 0.5, 1}},
+      {{0, 1, 0}, {0.5, 1, 1}, {1, 1, 0}}};
+  std::vector<std::vector<std::vector<double>>> q = {
+      {{0, 0, 0}, {0.5, 0, 1}, {1, 0, 0}},
+      {{0, 0.5, 1}, {0.5, 0.5, 2}, {1, 0.5, 1}},
+      {{0, 1, 0}, {0.5, 1, 1}, {1, 1, 0}}};
+  std::vector<std::vector<std::vector<double>>> x;
+
+  femib::mongo::plot_data p = {"12345", 0, u, q, x};
+  femib::mongo::save_plot_data("femib_test", p);
+  CHECK(true);
+}
