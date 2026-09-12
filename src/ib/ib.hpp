43fcc2aa31673a743126f4053dfa706df24963cd
Jacopo Tessera <jacopotessera@gmail.com>
2026-09-12 23:15:25 +0200

    feat: refactor ib advance and advance_stokes, extracting advance_common

--- src/ib/ib.hpp
+++ src/ib/ib.hpp
@@ -18,7 +18,11 @@ void init(ib_problem<T, d> &p, const femib::gauss::rule<T, d> &rule) {
   femib::stokes_t::init<T, d>(p.fluid, rule);
 }
 
-template <typename T, int d> void advance(ib_problem<T, d> &p) {
+template <typename T, int d>
+void advance_common(
+    ib_problem<T, d> &p,
+    const std::function<void(const Eigen::Matrix<T, Eigen::Dynamic, 1> &)>
+        &fluid_step) {
   std::vector<femib::types::dvec<T, d>> X =
       p.structure.X; // pre-advance positions
   std::vector<femib::types::dvec<T, d>> F = elastic_force<T, d>(p.structure);
@@ -30,7 +34,7 @@ template <typename T, int d> void advance(ib_problem<T, d> &p) {
   Eigen::Matrix<T, Eigen::Dynamic, 1> extra_rhs =
       spread_force<T, d>(p.fluid.V, X, F, p.structure.dS);
 
-  femib::stokes_t::advance<T, d>(p.fluid, extra_rhs);
+  fluid_step(extra_rhs);
 
   std::vector<femib::types::dvec<T, d>> U =
       interpolate_velocity<T, d>(p.fluid.V, p.fluid.solution.back(), X);
@@ -38,27 +42,23 @@ template <typename T, int d> void advance(ib_problem<T, d> &p) {
   advance_ring(p.structure, p.fluid.deltat, U);
 }
 
+template <typename T, int d> void advance(ib_problem<T, d> &p) {
+  advance_common<T, d>(
+      p, [&p](const Eigen::Matrix<T, Eigen::Dynamic, 1> &extra_rhs) {
+        femib::stokes_t::advance<T, d>(p.fluid, extra_rhs);
+      });
+}
+
 template <typename T, int d>
 void advance_navier_stokes(ib_problem<T, d> &p,
                            const femib::gauss::rule<T, d> &rule, T reynolds,
                            int max_picard_iters, T tol) {
-  std::vector<femib::types::dvec<T, d>> X = p.structure.X;
-  std::vector<femib::types::dvec<T, d>> F = elastic_force<T, d>(p.structure);
-
-  // spread_force expects a force density
-  for (auto &f : F) {
-    f /= p.structure.dS;
-  }
-  Eigen::Matrix<T, Eigen::Dynamic, 1> extra_rhs =
-      spread_force<T, d>(p.fluid.V, X, F, p.structure.dS);
-
-  femib::navier_stokes::advance<T, d>(p.fluid, rule, reynolds, max_picard_iters,
-                                      tol, extra_rhs);
-
-  std::vector<femib::types::dvec<T, d>> U =
-      interpolate_velocity<T, d>(p.fluid.V, p.fluid.solution.back(), X);
-
-  advance_ring(p.structure, p.fluid.deltat, U);
+  advance_common<T, d>(
+      p, [&p, &rule, reynolds, max_picard_iters,
+          tol](const Eigen::Matrix<T, Eigen::Dynamic, 1> &extra_rhs) {
+        femib::navier_stokes::advance<T, d>(p.fluid, rule, reynolds,
+                                            max_picard_iters, tol, extra_rhs);
+      });
 }
 
 } // namespace femib::ib
